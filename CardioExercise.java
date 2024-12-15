7e2abb49f1997294939b57c8968254df8d888a10
BurubugyotManalo <jeffrey.chan@g.batstate-u.edu.ph>
2024-12-16 01:46:15 +0800

    Update CardioExercise.java

--- CardioExercise.java
+++ CardioExercise.java
@@ -1,10 +1,10 @@
 public class CardioExercise extends Exercise {
     public CardioExercise() {
-        super("Cardio Exercise", 8.0); // Average calories burned per minute for cardio
+        super("Cardio Exercise", 8.0); // 8.0 calories burned per minute
     }
 
     @Override
     public double calculateCaloriesBurned(int duration) {
-        return duration * caloriesBurnedPerMinute;
+        return duration * caloriesBurnedPerMinute; // Calories burned = duration * rate per minute
     }
 }
