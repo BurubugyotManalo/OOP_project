aac4b5d5b97bcd3c1c571a61f103b01a7bb2bb23
BurubugyotManalo <jeffrey.chan@g.batstate-u.edu.ph>
2024-12-16 01:47:40 +0800

    Update StrengthExercise.java

--- StrengthExercise.java
+++ StrengthExercise.java
@@ -1,10 +1,10 @@
 public class StrengthExercise extends Exercise {
     public StrengthExercise() {
-        super("Strength Exercise", 5.0); // Average calories burned per minute for strength training
+        super("Strength Exercise", 5.0); // 5.0 calories burned per minute
     }
 
     @Override
     public double calculateCaloriesBurned(int duration) {
-        return duration * caloriesBurnedPerMinute;
+        return duration * caloriesBurnedPerMinute; // Calories burned = duration * rate per minute
     }
 }
